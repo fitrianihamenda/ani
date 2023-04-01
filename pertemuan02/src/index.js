d4ad1af561942406e108d3b3e4109b3a0664d3fc
fitrianihamenda <fitrianihamenda@gmail.com>
2023-04-01 09:14:33 -0700

    pertemuan02

--- pertemuan02/src/index.js
+++ pertemuan02/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Hello from './Hello';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Hello/>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
